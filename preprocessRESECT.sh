40c91891c657a1a2312c97d603562f4d83c834d6
David Kucher <mrdkucher@gmail.com>
2021-03-27 14:52:39 -0400

    Add FLAIR images to dataset as Case1XX.nii.gz, where XX corresponds to the case number

--- preprocessRESECT.sh
+++ preprocessRESECT.sh
@@ -25,47 +25,47 @@ fi
 # Create train dataset
 train_cases=(3 4 5 6 7 8 14 15 16 17 18 19 24 25 26 27)
 for i in "${train_cases[@]}"; do
-    if [[ ! -e "RESECT/preprocessed/train/fixed_images/Case$i.nii.gz" ]]; then
-        echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/train/moving_images/Case$i.nii.gz
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/train/fixed_images/Case$i.nii.gz
-        python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
-        c3d RESECT/preprocessed/train/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/train/moving_labels/Case$i.nii.gz
-        c3d RESECT/preprocessed/train/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/train/fixed_labels/Case$i.nii.gz
-        cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/train/landmarks/Case$i-MRI-beforeUS.tag
-        # Cleanup
-        rm lm_mri.txt lm_us.txt
-    fi;
+    echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/train/moving_images/Case$i.nii.gz
+    flair_index=$(seq -f "%02g" $i $i)
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/train/moving_images/Case1$flair_index.nii.gz
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/train/fixed_images/Case$i.nii.gz
+    python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
+    c3d RESECT/preprocessed/train/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/train/moving_labels/Case$i.nii.gz
+    c3d RESECT/preprocessed/train/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/train/fixed_labels/Case$i.nii.gz
+    cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/train/landmarks/Case$i-MRI-beforeUS.tag
+    # Cleanup
+    rm lm_mri.txt lm_us.txt
 done
 
 # Create val dataset
 val_cases=(1 12 13)
 for i in "${val_cases[@]}"; do
-    if [[ ! -e "RESECT/preprocessed/valid/fixed_images/Case$i.nii.gz" ]]; then
-        echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/valid/moving_images/Case$i.nii.gz
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/valid/fixed_images/Case$i.nii.gz
-        python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
-        c3d RESECT/preprocessed/valid/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/valid/moving_labels/Case$i.nii.gz
-        c3d RESECT/preprocessed/valid/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/valid/fixed_labels/Case$i.nii.gz
-        cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/valid/landmarks/
-        # Cleanup
-        rm lm_mri.txt lm_us.txt
-    fi;
+    echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/valid/moving_images/Case$i.nii.gz
+    flair_index=$(seq -f "%02g" $i $i)
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/valid/moving_images/Case1$flair_index.nii.gz
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/valid/fixed_images/Case$i.nii.gz
+    python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
+    c3d RESECT/preprocessed/valid/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/valid/moving_labels/Case$i.nii.gz
+    c3d RESECT/preprocessed/valid/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/valid/fixed_labels/Case$i.nii.gz
+    cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/valid/landmarks/
+    # Cleanup
+    rm lm_mri.txt lm_us.txt
 done
 
 # Create test dataset
 test_cases=(2 21 23)
 for i in "${test_cases[@]}"; do
-    if [[ ! -e "RESECT/preprocessed/test/fixed_images/Case$i.nii.gz" ]]; then
-        echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/test/moving_images/Case$i.nii.gz
-        c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/test/fixed_images/Case$i.nii.gz
-        python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
-        c3d RESECT/preprocessed/test/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/test/moving_labels/Case$i.nii.gz
-        c3d RESECT/preprocessed/test/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/test/fixed_labels/Case$i.nii.gz
-        cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/test/landmarks/
-        # Cleanup
-        rm lm_mri.txt lm_us.txt
-    fi;
+    echo "Resampling: RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz"
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-T1.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/test/moving_images/Case$i.nii.gz
+    flair_index=$(seq -f "%02g" $i $i)
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz RESECT/NIFTI/Case$i/MRI/Case$i-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/test/moving_images/Case1$flair_index.nii.gz
+    c3d RESECT/NIFTI/Case$i/US/Case$i-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o RESECT/preprocessed/test/fixed_images/Case$i.nii.gz
+    python landmarks_split_txt.py --inputtag RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag --savetxt lm
+    c3d RESECT/preprocessed/test/moving_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_mri.txt 1.25 -o RESECT/preprocessed/test/moving_labels/Case$i.nii.gz
+    c3d RESECT/preprocessed/test/fixed_images/Case$i.nii.gz -scale 0 -landmarks-to-spheres lm_us.txt 1.25 -o RESECT/preprocessed/test/fixed_labels/Case$i.nii.gz
+    cp RESECT/NIFTI/Case$i/Landmarks/Case$i-MRI-beforeUS.tag  RESECT/preprocessed/test/landmarks/
+    # Cleanup
+    rm lm_mri.txt lm_us.txt
 done
