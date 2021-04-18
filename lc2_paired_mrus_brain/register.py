18b4f99133788bf706a0a5be1b370a8a01234d54
David Kucher <mrdkucher@gmail.com>
2021-04-18 16:46:52 -0400

    Fixed patches

--- lc2_paired_mrus_brain/register.py
+++ lc2_paired_mrus_brain/register.py
@@ -54,8 +54,6 @@ def reg(args):
         MOVING_PATH, image_size=args.image_size)
     fixed_image, fixed_image_aff = load_preprocess_image(
         FIXED_PATH, image_size=args.image_size)
-    fi = fixed_image.numpy()
-    print("Percent nonzero:", np.sum(fi > 0) / np.product(fi.shape))
 
     if use_labels:
         # load and prepreprocess fixed and moving landmarks (images)
