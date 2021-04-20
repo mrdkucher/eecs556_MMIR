174492a4c65133b5cdacd437cc326db135c378f3
Ellen Michelle Yeats <yeatsem@gl-login2.arc-ts.umich.edu>
2021-04-20 19:13:40 -0400

    ssc updates

--- regDefSSC.sh
+++ regDefSSC.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
 mmirpath="/home/yeatsem/Proj556/eecs556_MMIR" #put the path to the local version of the github repo here
-itkpath="/home/yeatsem/itk/bin" #put the path to itk binary folder here
+c3dpath="/home/yeatsem/itk/bin" #put the path to c3d binary folder here
 resectpath="/home/yeatsem/Proj556/RESECT" #put the path to RESECT data here
-deedspath="/home/yeatsem/Proj556/deeds" #put path to the deeds binaries here
 
-outfile="/home/yeatsem/Proj556/DefAlone-4Levels-Results.txt"
+outfile="reg_results_deform_SSC.txt"
 
-g="4x3x2x1" #grid spacing
-l="10x9x7x6" #maximum search radius
-q="4x3x2x1" #quantization
-  
-for c in {1..27}
+g_aff="6x5x4x3" #grid spacing
+l_aff="4" #number of levels
+L_aff="7x6x5x4"
+q_aff="4x3x2x1"
+
+l_def="4" #number of levels
+g_def="6x5x4x3" #grid spacing
+L_def="7x6x5x4" #maximum search radius
+q_def="4x3x2x1" #quantization
+a="1.6"
+
+echo "affine params"
+echo ${l_aff} >> ${outfile}
+echo ${g_aff} >> ${outfile}
+echo ${L_aff} >> ${outfile}
+echo ${q_aff} >> ${outfile}
+
+echo "deformable params"
+echo ${l_def} >> ${outfile}
+echo ${g_def} >> ${outfile}
+echo ${L_def} >> ${outfile}
+echo ${q_def} >> ${outfile}
+echo ${a} >> ${outfile}
+
+for c in {1..8} {12..19} 21 {23..27}
 do
   #preprocess the RESECT data
-  cd ${itkpath}
-  ./c3d ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz ${resectpath}/NIFTI/Case${c}/MRI/Case${c}-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz
-
-  ./c3d ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz
+  cd ${c3dpath}
+  ./c3d  ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz  ${resectpath}/NIFTI/Case${c}/MRI/Case${c}-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz
+  ./c3d  ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o  ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz
 
   #save landmark coordinates as .txt
   cd ${mmirpath}
-  python landmarks_split_txt.py --inputtag ${resectpath}/NIFTI/Case${c}/Landmarks/Case${c}-MRI-beforeUS.tag --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}_lm
-  
-  #save landmark coordinates as .txt
-  cd ${mmirpath}
-  python landmarks_split_txt.py --inputtag ${resectpath}/NIFTI/Case${c}/Landmarks/Case${c}-MRI-beforeUS.tag --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}_lm
+  python landmarks_split_txt_SSC.py --inputtag ${resectpath}/NIFTI/Case${c}/Landmarks/Case${c}-MRI-beforeUS.tag --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}_lm
 
   #generate MRI voxelized landmarks
-  cd ${itkpath}
+  cd ${c3dpath}
   ./c3d ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -scale 0 -landmarks-to-spheres ${resectpath}/NIFTI/Case${c}/Case${c}_lm_mri.txt 1 -o ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz
 
   # generate US voxelized landmarks
@@ -36,29 +50,32 @@ do
 
   echo "Calculating initial TRE"
   cd ${mmirpath}
-  iniTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-prelim)
+  iniTRE=$(python landmarks_centre_mass_SSC.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-prelim)
   
   # run deformable deeds
-  cd ${deedspath}
-  ./deedsBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz  -G ${g} -L ${l} -Q ${q} -O ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone -S ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz 
+  cd deeds
+  defTime=$(./deedsBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz  -G ${g_def} -l ${l_def} -L ${L_def} -Q ${q_def} -a ${a} -O ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone -S ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz) 
 
   echo "Calculating TRE for deformable registration"
-  cd ${mmirpath}
-  defTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-deeds_alone)
+  cd ..
+  defTRE=$(python landmarks_centre_mass_SSC.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-deeds_alone)
 
   #do a linear fit
-  cd ${deedspath}
-  ./linearBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone_deformed.nii.gz -S ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone_deformed_seg.nii.gz -R 1 -O ${resectpath}/NIFTI/Case${c}/affine${c}_deeds_alone 
+  cd deeds
+  defaffTime=$(./linearBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_alone_deformed.nii.gz  -G ${g_aff} -l ${l_aff} -L ${L_aff} -Q ${q_aff}  -S ${resectpath}//NIFTI/Case${c}/Case${c}-deeds_alone_deformed_seg.nii.gz -R 1 -O ${resectpath}/NIFTI/Case${c}/affine${c}_deeds_alone) 
 
   echo "Calculating TRE for nonlinear + linear fit"
-  cd ${deedspath} 
-  defaffTRE=$(python landmarks_centre_mass.py --inputnii resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii resectpath}/NIFTI/Case${c}/affine${c}_deeds_alone_deformed_seg.nii.gz --savetxt resectpath}/NIFTI/Case${c}/Case${c}-results-deeds_lin)
+  cd ..
+  defaffTRE=$(python landmarks_centre_mass_SSC.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/affine${c}_deeds_alone_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-deeds_lin)
 
 echo ${c} >> ${outfile}
 echo ${iniTRE} >> ${outfile}
 echo ${defTRE} >> ${outfile}
 echo ${defaffTRE} >> ${outfile}
+echo ${defTime} >> ${outfile}
+echo ${defaffTime} >> ${outfile}
 
 done  
   
 exit
+
--- regSSC.sh
+++ regSSC.sh
@@ -1,63 +1,68 @@
 #!/bin/bash
-  
-#put the patient case number here
+
 mmirpath="/home/yeatsem/Proj556/eecs556_MMIR" #put the path to the local version of the github repo here
-itkpath="/home/yeatsem/itk/bin" #put the path to itk binary folder here
+c3dpath="/home/yeatsem/itk/bin" #put the path to c3d binary folder here
 resectpath="/home/yeatsem/Proj556/RESECT" #put the path to RESECT data here
-deedspath="/home/yeatsem/Proj556/deeds"
 
-outfile="/home/yeatsem/Proj556/DefaultParams-Results.txt"
+outfile="reg_results_SSC.txt"
 
-g="5x4x3x2" #grid spacing
-l="8x7x6x5" #maximum search radius
-q="5x4x3x2" #quantization
-  
+g_aff="6x5x4x3" #grid spacing
+l_aff="4" #number of levels
+L_aff="7x6x5x4"
+q_aff="4x3x2x1"
+
+l_def="4" #number of levels
+g_def="6x5x4x3" #grid spacing
+L_def="7x6x5x4" #maximum search radius
+q_def="4x3x2x1" #quantization
+a="1.6"
+
 for c in {1..8} {12..19} 21 {23..27}
 do
   #preprocess the RESECT data
-  cd ${itkpath}
-  ./c3d ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz ${resectpath}/NIFTI/Case${c}/MRI/Case${c}-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz
+  cd ${c3dpath}
+  ./c3d ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz  ${resectpath}/NIFTI/Case${c}/MRI/Case${c}-FLAIR.nii.gz -reslice-identity -resample-mm 0.5x0.5x0.5mm -o  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz
 
-  ./c3d ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz
+  ./c3d  ${resectpath}/NIFTI/Case${c}/US/Case${c}-US-before.nii.gz -resample-mm 0.5x0.5x0.5mm -o  ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz
 
   #save landmark coordinates as .txt
   cd ${mmirpath}
-  python landmarks_split_txt.py --inputtag ${resectpath}/NIFTI/Case${c}/Landmarks/Case${c}-MRI-beforeUS.tag --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}_lm
+  python landmarks_split_txt.py --inputtag  ${resectpath}/NIFTI/Case${c}/Landmarks/Case${c}-MRI-beforeUS.tag --savetxt  ${resectpath}/NIFTI/Case${c}/Case${c}_lm
 
   #generate MRI voxelized landmarks
-  cd ${itkpath}
-  ./c3d ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -scale 0 -landmarks-to-spheres ${resectpath}/NIFTI/Case${c}/Case${c}_lm_mri.txt 1 -o ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz
+  cd ${c3dpath}
+  ./c3d ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -scale 0 -landmarks-to-spheres  ${resectpath}/RESECT_SSC/NIFTI/Case${c}/Case${c}_lm_mri.txt 1 -o  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz
 
   # generate US voxelized landmarks
-  ./c3d ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -scale 0 -landmarks-to-spheres ${resectpath}/NIFTI/Case${c}/Case${c}_lm_us.txt 1 -o ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz
+  ./c3d  ${resectpath}/RESECT_SSC/NIFTI/Case${c}/Case${c}-US.nii.gz -scale 0 -landmarks-to-spheres  ${resectpath}/NIFTI/Case${c}/Case${c}_lm_us.txt 1 -o  ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz
 
   echo "Calculating initial TRE"
   cd ${mmirpath}
-  iniTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-prelim)
+  iniTRE=$(python landmarks_centre_mass_SSC.py --inputnii  ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz --savetxt  ${resectpath}/NIFTI/Case${c}/Case${c}-prelim)
 
   # do preliminary linear fit
-  cd ${deedspath}
-  ./linearBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -G ${g} -L ${l} -Q ${q} -R 1 -S ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz -O ${resectpath}/NIFTI/Case${c}/affine${c} 
+  cd deeds
+  ./linearBCV -F  ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -G ${g} -L ${l} -Q ${q} -R 1 -S  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz -O  ${resectpath}/NIFTI/Case${c}/affine${c} 
 
   echo "Calculating TRE for linear fit"
-  cd ${mmirpath}
-  affTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/affine${c}_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-L)
+  cd ..
+  affTRE=$(python landmarks_centre_mass.py --inputnii  ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii  ${resectpath}/NIFTI/Case${c}/affine${c}_deformed_seg.nii.gz --savetxt  ${resectpath}/NIFTI/Case${c}/Case${c}-results-L)
 
   # run deformable deeds
-  cd ${deedspath}
-  ./deedsBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -G ${g} -L ${l} -Q ${q} -O ${resectpath}/NIFTI/Case${c}/Case${c}-deeds -S ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz -A ${resectpath}/NIFTI/Case${c}/affine${c}_matrix.txt 
+  cd deeds
+  ./deedsBCV -F  ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI_in_US.nii.gz -G ${g} -L ${l} -Q ${q} -O  ${resectpath}/NIFTI/Case${c}/Case${c}-deeds  ${resectpath}/NIFTI/Case${c}/Case${c}-MRI-landmarks.nii.gz -A  ${resectpath}/NIFTI/Case${c}/affine${c}_matrix.txt 
 
   echo "Calculating TRE for linear + nonlinear fit"
-  cd ${mmirpath}
-  affdefTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-NL)
+  cd ..
+  affdefTRE=$(python landmarks_centre_mass.py --inputnii  ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii  ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed_seg.nii.gz --savetxt  ${resectpath}/NIFTI/Case${c}/Case${c}-results-NL)
 
   #do second linear fit
-  cd ${deedspath}
-  ./linearBCV -F ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed.nii.gz -G ${g} -L ${l} -Q ${q} -S ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed_seg.nii.gz -R 1 -O ${resectpath}/NIFTI/Case${c}/affine${c}_2 
+  cd deeds
+  ./linearBCV -F  ${resectpath}/NIFTI/Case${c}/Case${c}-US.nii.gz -M  ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed.nii.gz -G ${g} -L ${l} -Q ${q} -S  ${resectpath}/NIFTI/Case${c}/Case${c}-deeds_deformed_seg.nii.gz -R 1 -O  ${resectpath}/NIFTI/Case${c}/affine${c}_2 
 
   echo "Calculating TRE for linear + nonlinear + linear fit"
-  cd ${mmirpath} 
-  affdefaffTRE=$(python landmarks_centre_mass.py --inputnii ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii ${resectpath}/NIFTI/Case${c}/affine${c}_2_deformed_seg.nii.gz --savetxt ${resectpath}/NIFTI/Case${c}/Case${c}-results-LNL)
+  cd ..
+  affdefaffTRE=$(python landmarks_centre_mass.py --inputnii  ${resectpath}/NIFTI/Case${c}/Case${c}-US-landmarks.nii.gz --movingnii  ${resectpath}/NIFTI/Case${c}/affine${c}_2_deformed_seg.nii.gz --savetxt  ${resectpath}/NIFTI/Case${c}/Case${c}-results-LNL)
 
 echo ${c} >> ${outfile}
 echo ${iniTRE} >> ${outfile}
